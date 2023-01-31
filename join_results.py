7afeb32ef8a72ab7ec9fc8fa7be782ca89542e54
Rainer Simon <rainer@rainersimon.io>
2023-01-31 11:19:57 +0100

    Stats logging

--- join_results.py
+++ join_results.py
@@ -29,7 +29,12 @@ results_pl = read_csv(PATH_PLEIADES)
 Script starts here!
 """
 
+total = 0
+complete_misses = 0
+
 for idx, row in enumerate(bm_places):
+  total += 1
+
   row_gn = results_gn[idx]
   row_wp = results_wp[idx]
   row_pl = results_pl[idx]
@@ -37,6 +42,17 @@ for idx, row in enumerate(bm_places):
   row.update(row_gn)
   row.update(row_wp)
   row.update(row_pl)
+
+  no_gn = len(row['GeoNames URI'].strip()) == 0
+  no_wp = len(row['Wikipedia URI'].strip()) == 0
+  no_pl = len(row['Pleiades URI'].strip()) == 0
+
+  if no_gn and no_wp and no_pl:
+    complete_misses += 1
+
+# Log stats
+print('Total rows: ' + str(total))
+print('Misses: ' + str(complete_misses))
 
 csv_columns = [
   'System ID',
@@ -76,6 +92,6 @@ with open('./reconciled_all.csv', 'w') as csvfile:
   writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
   writer.writeheader()
 
-  for row in bm_places  :
+  for row in bm_places:
     writer.writerow(row)
 
